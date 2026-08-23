c2f5b4b886d3bbd88308f1b43d91852863b09bfc
Your Name <your@email.com>
2026-08-23 22:49:01 +0100

    Configured core libraries for time-series forceasting and modelling

--- SKU demand time series forecasting.py
+++ SKU demand time series forecasting.py
@@ -1,1 +1,8 @@
 #SKU demand time series forecasting
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
+from sklearn.metrics import mean_absolute_percentage_error
